4fb26b5bc67d3090dad6f4fa53e33a8de021272a
UnEmotioneD <blackeagle10@icloud.com>
2025-07-21 18:03:42 +0900

    Test opening file
    open file chooser
    write path to db

--- src/main/java/com/mcs/modelsearcher/MainApplication.java
+++ src/main/java/com/mcs/modelsearcher/MainApplication.java
@@ -1,6 +1,8 @@
 package com.mcs.modelsearcher;
 
+import com.mcs.modelsearcher.file.controller.FileController;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,6 +12,13 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FileController fileController = new FileController();
+        fileController.openFile(stage);
+
+        // if choosing file fails
+        System.out.println("No valid file selected");
+        Platform.exit(); // gracefully exit
+        System.exit(0); // fully exit
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
