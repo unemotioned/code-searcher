6e18f029ac5687f5c9d8a55637bb2ad13cfda3eb
UnEmotioneD <blackeagle10@icloud.com>
2025-07-21 14:19:44 +0900

    Initial commit

---
+++ src/main/java/com/mcs/modelsearcher/MainController.java
@@ -0,0 +1,19 @@
+package com.mcs.modelsearcher;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label filePath;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    protected void onSelectFileClick() {
+        filePath.setText("Select file button clicked!");
+    }
+}
